482ecff5c0d8cb224ff5c922e2632dde35fe3142
Сергей Попов <about.the.man@gmail.com>
2024-11-29 00:18:19 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 import React, { useEffect } from 'react';
 
 function App() {
+  const [user, setUser] = useState(null); // Состояние для хранения информации о пользователе
+
 
   useEffect(() => {
     // Инициализируем Telegram Web Apps SDK
     const tg = window.Telegram.WebApp;
     tg.expand(); // Разворачиваем приложение на весь экран
-// Изменение заголовка приложения
-tg.MainButton.text = "Нажми меня!";
-tg.MainButton.show();
+
+        // Получаем информацию о пользователе
+        const userData = tg.initDataUnsafe.user; // Получаем данные о пользователе
+        setUser(userData); // Сохраняем данные о пользователе в состоянии
+
+
     
 
   }, []);
@@ -32,6 +37,18 @@ tg.MainButton.show();
           Learn React
         </a>
       </header>
+        {/* Отображение информации о пользователе */}
+        {user ? (
+        <div>
+          <h2>Информация о пользователе:</h2>
+          <p>Имя: {user.first_name}</p>
+          <p>Фамилия: {user.last_name}</p>
+          <p>Username: {user.username}</p>
+          <p>ID: {user.id}</p>
+        </div>
+      ) : (
+        <p>Загрузка информации о пользователе...</p>
+      )}
     </div>
   );
 }
