5ac6d29de16d4de91987917817fd5a4a20b79a8c
Сергей Попов <about.the.man@gmail.com>
2024-11-29 00:21:53 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -10,19 +10,50 @@ function App() {
   useEffect(() => {
     // Инициализируем Telegram Web Apps SDK
     const tg = window.Telegram.WebApp;
-    tg.expand(); // Разворачиваем приложение на весь экран
+    //tg.expand(); // Разворачиваем приложение на весь экран
 
-        // Получаем информацию о пользователе
+    // Получаем информацию о пользователе
+    const fetchUserData = async () => {
+      try {
         const userData = tg.initDataUnsafe.user; // Получаем данные о пользователе
+        if (!userData) {
+          throw new Error('Не удалось получить данные о пользователе');
+        }
         setUser(userData); // Сохраняем данные о пользователе в состоянии
+      } catch (err) {
+        setError(err.message); // Сохраняем сообщение об ошибке в состоянии
+      }
+    };
 
-
+    fetchUserData();
     
 
   }, []);
 
   return (
     <div className="App">
+
+      {/* Отображение ошибок */}
+      {error && (
+        <div style={{ color: 'red' }}>
+          <h2>Ошибка:</h2>
+          <p>{error}</p>
+        </div>
+      )}
+
+      {/* Отображение информации о пользователе */}
+      {user ? (
+        <div>
+          <h2>Информация о пользователе:</h2>
+          <p>Имя: {user.first_name}</p>
+          <p>Фамилия: {user.last_name}</p>
+          <p>Username: {user.username}</p>
+          <p>ID: {user.id}</p>
+        </div>
+      ) : (
+        <p>Загрузка информации о пользователе...</p>
+      )}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -37,18 +68,6 @@ function App() {
           Learn React
         </a>
       </header>
-        {/* Отображение информации о пользователе */}
-        {user ? (
-        <div>
-          <h2>Информация о пользователе:</h2>
-          <p>Имя: {user.first_name}</p>
-          <p>Фамилия: {user.last_name}</p>
-          <p>Username: {user.username}</p>
-          <p>ID: {user.id}</p>
-        </div>
-      ) : (
-        <p>Загрузка информации о пользователе...</p>
-      )}
     </div>
   );
 }
