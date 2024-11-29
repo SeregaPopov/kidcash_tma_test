f915f9581b93dbe3401ebb9d8e82b5f06eae7a05
Сергей Попов <about.the.man@gmail.com>
2024-11-29 10:05:05 +0300

    modified:   src/services/authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://node02.msk.prototypes.ventures:50105/api/';
+const API_URL = 'http://node02.msk.prototypes.ventures:50105/api';
 
 export const authenticateUser = async () => {
   const tg = window.Telegram.WebApp;
@@ -8,13 +8,13 @@ export const authenticateUser = async () => {
   const initDataUnsafe = tg.initDataUnsafe;
 
   try {
-    const response = await axios.post(`${API_URL}/auth/telegram/`, {
+    const response = await axios.post('${API_URL}/auth/telegram/', {
       initData,
     });
 
     // Сохраняем токен в localStorage или context
     const token = response.data.access;
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    axios.defaults.headers.common['Authorization'] = 'Bearer ${token}';
 
     return response.data;
   } catch (error) {
